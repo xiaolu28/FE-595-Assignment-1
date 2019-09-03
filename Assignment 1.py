d872330e5df960651daa12a88b2d40e8905267e5
xiaolu28 <45919641+xiaolu28@users.noreply.github.com>
2019-09-02 21:12:03 -0400

    Add files via upload

---
+++ Assignment 1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep  2 20:59:18 2019
+
+@author: xiaolu
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import math 
+### create a range from -2*pi to 2 *pi
+t = np.linspace(-2 * math.pi, 2 * math.pi, 300)
+### plot sine and cosine
+plt.plot(t, np.sin(t), label = "Sine")
+plt.plot(t, np.cos(t), label = "Cosine")
+### create title and legend for graph
+plt.title("Graph of sine and cosine")
+plt.legend()
+plt.xticks((-np.pi*2,-np.pi*3/2,-np.pi,-np.pi/2, 0,
+            np.pi/2,np.pi,np.pi*3/2,np.pi*2),
+            ('-2π','-3π/2','-π','-π/2','0','π/2','π','3π/2','2π'))
